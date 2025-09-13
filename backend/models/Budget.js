25c625370bd1a61330eb96b47725049e62b04397
Nityashree SP <nityashreesp02@gmail.com>
2025-09-13 20:37:48 +0530

    Remove all folders from repo

--- backend/models/Budget.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const budgetSchema = new mongoose.Schema({
-  total: { type: Number, required: true },
-  year: { type: Number, required: true },
-});
-
-module.exports = mongoose.model("Budget", budgetSchema);
--- backend/models/Department.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const departmentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  allocated: { type: Number, required: true },
-  budgetId: { type: mongoose.Schema.Types.ObjectId, ref: "Budget" },
-});
-
-module.exports = mongoose.model("Department", departmentSchema);
